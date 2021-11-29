34f6c07172c640f19595daf0c4b0a08f6bbcc627
ekaterina <koshake1@gmail.com>
2021-11-30 01:03:06 +0300

    feature:firebase

---
+++ app/src/main/java/com/koshake1/pulseapp/db/DatabaseProvider.kt
@@ -0,0 +1,9 @@
+package com.koshake1.pulseapp.db
+
+import com.koshake1.pulseapp.model.data.Note
+import kotlinx.coroutines.flow.Flow
+
+interface DatabaseProvider {
+    fun observeAllNotes() : Flow<List<Note>>
+    suspend fun addNote(newNote : Note)
+}
---
+++ app/src/main/java/com/koshake1/pulseapp/db/FireBaseProvider.kt
@@ -0,0 +1,34 @@
+package com.koshake1.pulseapp.db
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import com.koshake1.pulseapp.model.data.Note
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+private const val NOTES_COLLECTION = "notes"
+private const val USERS_COLLECTION = "users"
+const val TAG = "FireStoreDatabase"
+
+class FireBaseProvider (
+    private val db: FirebaseFirestore,
+    private val firebaseAuth: FirebaseAuth
+) : DatabaseProvider {
+
+    private val result = MutableStateFlow<List<Note>?>(null)
+    private var subscribedOnDb = false
+
+    override fun observeAllNotes(): Flow<List<Note>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addNote(newNote: Note) {
+
+    }
+}
